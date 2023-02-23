61e14dcba95ac06d53e091379554ba57d7d2afa7
Raphael Batagini <raphabatagini@gmail.com>
2023-02-23 10:18:08 -0300

    feat: cover deterministicPartitionKey method with more tests

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,36 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns the same partitionKey when it doesn't exceed maximum partition key length", () => {
+    const validPartitionKeyObject = {
+      partitionKey: "some_valid_length_key",
+    };
+    const trivialKey = deterministicPartitionKey(validPartitionKeyObject);
+    expect(trivialKey).toBe(validPartitionKeyObject.partitionKey);
+  });
+
+  it("Returns the same partitionKey as a string when the input is a number", () => {
+    const numericPartitionKeyObject = {
+      partitionKey: 100,
+    };
+    const trivialKey = deterministicPartitionKey(numericPartitionKeyObject);
+    expect(typeof trivialKey).toBe("string");
+  });
+
+  it("Returns a 128 length string when the input doesn't have a partitionKey property", () => {
+    const invalidObject = {
+      someInvalidKey: 'something'
+    };
+    const trivialKey = deterministicPartitionKey(invalidObject);
+    expect(trivialKey.length).toBe(128);
+  });
+
+  it("Returns a 128 length string when the input is longer than that", () => {
+    const longPartitionKeyObject = {
+      partitionKey: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu ornare risus. Maecenas eget odio quam. Nunc sagittis et mi quis cursus. Quisque tempor neque eu est porttitor, ac pulvinar quam gravida. In ullamcorper, libero id porta dignissim, velit turpis.",
+    };
+    const trivialKey = deterministicPartitionKey(longPartitionKeyObject);
+    expect(trivialKey.length).toBe(128);
+  });
 });
