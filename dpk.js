65949d1429a227e8db78ef39a31b16da840a85bc
Raphael Batagini <raphabatagini@gmail.com>
2023-02-23 10:31:35 -0300

    refactor: improve deterministicPartitionKey method

--- dpk.js
+++ dpk.js
@@ -5,24 +5,27 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (!event.partitionKey) {
+    candidate = hashData(JSON.stringify(event));
   }
+
+  if (!candidate) {
+    candidate = event.partitionKey;
+  }
+
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = hashData(candidate);
   }
+
   return candidate;
 };
+
+const hashData = (data) => crypto.createHash("sha3-512").update(data).digest("hex");
--- dpk.test.js
+++ dpk.test.js
@@ -10,31 +10,31 @@ describe("deterministicPartitionKey", () => {
     const validPartitionKeyObject = {
       partitionKey: "some_valid_length_key",
     };
-    const trivialKey = deterministicPartitionKey(validPartitionKeyObject);
-    expect(trivialKey).toBe(validPartitionKeyObject.partitionKey);
+    const partitionKey = deterministicPartitionKey(validPartitionKeyObject);
+    expect(partitionKey).toBe(validPartitionKeyObject.partitionKey);
   });
 
   it("Returns the same partitionKey as a string when the input is a number", () => {
     const numericPartitionKeyObject = {
       partitionKey: 100,
     };
-    const trivialKey = deterministicPartitionKey(numericPartitionKeyObject);
-    expect(typeof trivialKey).toBe("string");
+    const partitionKey = deterministicPartitionKey(numericPartitionKeyObject);
+    expect(typeof partitionKey).toBe("string");
   });
 
   it("Returns a 128 length string when the input doesn't have a partitionKey property", () => {
     const invalidObject = {
       someInvalidKey: 'something'
     };
-    const trivialKey = deterministicPartitionKey(invalidObject);
-    expect(trivialKey.length).toBe(128);
+    const partitionKey = deterministicPartitionKey(invalidObject);
+    expect(partitionKey.length).toBe(128);
   });
 
   it("Returns a 128 length string when the input is longer than that", () => {
     const longPartitionKeyObject = {
       partitionKey: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu ornare risus. Maecenas eget odio quam. Nunc sagittis et mi quis cursus. Quisque tempor neque eu est porttitor, ac pulvinar quam gravida. In ullamcorper, libero id porta dignissim, velit turpis.",
     };
-    const trivialKey = deterministicPartitionKey(longPartitionKeyObject);
-    expect(trivialKey.length).toBe(128);
+    const partitionKey = deterministicPartitionKey(longPartitionKeyObject);
+    expect(partitionKey.length).toBe(128);
   });
 });
